d1f24b2ad2cf8969348f6ab9a2bee157f0348f9d
Muhammad Galhoum <56798376+Muhammad-Galhoum@users.noreply.github.com>
2021-04-15 22:10:38 +0200

    Lessons tasks 024 through 025

---
+++ Lessons assignments from Lesson 024 to 025.py
@@ -0,0 +1,36 @@
+# تكليفات الدروس من الدرس 024 إلى 025
+# -----------------------------------------------
+
+# Task_1
+
+my_tuple = "Muhammad",
+for item in my_tuple:
+    print(f"\"{item}\"")
+print(type(my_tuple))
+
+
+# Task_2
+
+friends = ("Osama", "Ahmed", "Sayed")
+friends = ("Elzero", "Ahmed", "Sayed")
+print(friends)
+print(type(friends))
+print(len(friends))
+
+
+# Task_3
+
+nums = (1, 2, 3)
+letters = ("A", "B", "C")
+nums_and_letters = nums + letters
+print(nums_and_letters)
+print(len(nums_and_letters))
+
+
+# Task_4
+
+my_tuple = (1, 2, 3, 4)
+a, b, _, c = my_tuple
+print(f"{a}")
+print(f"{b}")
+print(f"{c}")
