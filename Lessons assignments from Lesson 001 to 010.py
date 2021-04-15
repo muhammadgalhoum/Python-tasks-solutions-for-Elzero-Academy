706c3e8c23af55e2a1b3961e0062d256e5bd49aa
Muhammad Galhoum <56798376+Muhammad-Galhoum@users.noreply.github.com>
2021-04-15 22:07:18 +0200

    Lessons tasks 001 through 010

---
+++ Lessons assignments from Lesson 001 to 010.py
@@ -0,0 +1,38 @@
+# تكليفات الدروس من الدرس 001 إلى 010
+# -----------------------------------------------
+
+# Task_1
+
+# This assignment and the following are writen by me
+# Comment_2
+# Comment_3
+# Comment_4
+# Comment_5
+# Comment_6
+
+
+# Task_2
+
+my_name = "Muhammad"
+my_age = "23"
+my_country = "Egypt"
+
+
+# Task_3
+
+print(f"Name: {my_name}")
+print(f"Age: {my_age}")
+print(f"Country: {my_country}")
+
+
+# Task_4
+
+print(
+    f"Hello, My Name Is {my_name} And I\'m {my_age} Years Old and I Live in {my_country}")
+
+
+# Task_5
+
+print(type(my_name))
+print(type(my_age))
+print(type(my_country))
