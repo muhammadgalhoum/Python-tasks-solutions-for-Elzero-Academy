9b059c14ad18711a42ef568098ad3e0833b7e6e9
Muhammad Galhoum <56798376+Muhammad-Galhoum@users.noreply.github.com>
2021-04-15 22:09:36 +0200

    Lessons tasks 019 through 020

---
+++ Lessons assignments from Lesson 019 to 020.py
@@ -0,0 +1,45 @@
+# تكليفات الدروس من الدرس 019 إلى 020
+# -----------------------------------------------
+
+# Task_1
+
+integer_num = 6
+float_num = 6.9
+complex_num = 6 + 5j
+print(
+    f"The type of integer_num variable which equal to {integer_num} Is : {type(integer_num)}")
+print(
+    f"The type of float_num variable which equal to  {float_num} Is : {type(float_num)}")
+print(
+    f"The type of complex_num variable which equal to = {complex_num} Is : {type(complex_num)}")
+
+
+# Task_2
+
+my_complex_number = 1 + 2j
+print(my_complex_number.real)
+print(my_complex_number.imag)
+
+
+# Task_3
+
+num = 10
+print(f"{num:.10f}")
+
+
+# Task_4
+
+num = 159.650
+turn_num = int(num)
+print(turn_num)
+print(type(turn_num))
+
+
+# Task_5
+
+# I wrote the  following Lines  as comments to prevent any Error to happen
+# 100 - 115 = -15
+# 50 * 30 = 1500
+# 21 % 4 = 1
+# 110 / 11 = 10
+# 97 // 20 = 4
